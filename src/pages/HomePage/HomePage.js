d3426099380cf1eff50654fa4c504d678d901fec
Anburaj2005 <avganburaj@gmail.com>
2024-05-01 12:31:37 +0530

    Homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,21 +1,21 @@
-import React, {useState, useEffect} from 'react';
+// import React, {useState, useEffect} from 'react';
 import { Outlet } from "react-router-dom";
 import "./HomePage.css";
 import Sidebar from "../../components/Sidebar/Sidebar";
 
 const HomePage = () => {
-  const [greetText, setGreetText] = useState("");
-  const currentDate = new Date();
-  const day = currentDate.toLocaleDateString('default', {weekday: 'long'});
-  const month = currentDate.toLocaleString('default', {month: 'long'});
-  const date = `${day}, ${month} ${currentDate.getDate()}, ${currentDate.getFullYear()}`;
+  // const [greetText, setGreetText] = useState("");
+  // const currentDate = new Date('');
+  // const day = currentDate.toLocaleDateString('default', {weekday: 'long'});
+  // const month = currentDate.toLocaleString('default', {month: 'long'});
+  // const date = `${day}, ${month} ${currentDate.getDate()}, ${currentDate.getFullYear()}`;
 
-  useEffect(() => {
-    let currentHour = currentDate.getHours();
-    if(currentHour < 12) setGreetText("Good Morning!")
-    else if(currentHour < 18) setGreetText("Good Afternoon!");
-    else setGreetText("Good Evening!");
-  },[])
+  // useEffect(() => {
+  //   let currentHour = currentDate.getHours();
+  //   if(currentHour < 12) setGreetText("Good Morning!")
+  //   else if(currentHour < 18) setGreetText("Good Afternoon!");
+  //   else setGreetText("Good Evening!");
+  // },[currentDate])
 
   return (
     <div className='app flex'>
@@ -25,10 +25,10 @@ const HomePage = () => {
           <div className='container w-100'>
             <div className='header-content flex align-center justify-between text-white py-3'>
               <div className='greetings'>
-                <h3 className='fw-6'>{greetText}</h3>
+                {/* <h3 className='fw-6'>{greetText}</h3> */}
               </div>
               <div className='date'>
-                <span className='text-uppercase fs-13 fw-4'>{date}</span>
+                {/* <span className='text-uppercase fs-13 fw-4'>{date}</span> */}
               </div>
             </div>
           </div>
